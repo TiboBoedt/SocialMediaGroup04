609e63251cf06648fe8b653aaed200bc6fc8c1c0
tboedt <Tibo.Boedt@UGent.be>
2022-03-10 17:28:47 +0100

    Scrapping

---
+++ Scrapping/Bitcoin_scrapping.R
@@ -0,0 +1,21 @@
+get_token()
+
+#strings
+search.string <- c("#bitcoin", "BTC", "bitcoin", "#BTC")
+
+for(i in 1:length(search.string)){
+  search <- search_tweets(search.string[i], n = 1000, lang = "en", include_rts = F)
+  if(i == 1){
+    tweets = search
+  }
+  else {
+    tweets = rbind(tweets, search)
+  }
+}
+
+#csv inladen
+bitcoin_csv <- read_twitter_csv("Bitcoin.csv")
+#tweets toevegen
+tweets <- rbind(bitcoin_csv, tweets)
+#csv writen
+write_as_csv(tweets, "Bitcoin")
